c64590f11fc81524b85f4dc95edece432b145f63
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2021-01-07 14:14:24 +0900

    1:07:55

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from './axios';
 
-function row() {
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results)
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.log(movies);
+
+
+
     return (
-        <div>
-            
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
+            {/*df*/}
         </div>
     )
 }
 
-export default row
+export default Row
